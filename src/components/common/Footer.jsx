b91483a7e8ab857dd19ebd7bdcbfb5a344ae1556
brunomry <brunomadozzo@gmail.com>
2024-07-02 04:53:33 -0300

    correccion de scroll horizontal en mobile

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark w-100 pt-3">
+    <footer className="bg-dark pt-3">
       <p className="text-center text-white pb-1">
         &copy;Todos los derechos reservados. 2024. Diseñado por{" "}
         <a href="https://www.linkedin.com/in/bruno-madozzo" target="_blank" className="text-decoration-none text-warning">
